38fb7624dc121ffe5a5ea4dccfeb6fe30b39adff
Cameron Irmas <cirmas@protonmail.com>
2020-08-04 12:36:52 -0700

    Refactor calls to model and add summary function

--- stats/stats.py
+++ stats/stats.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from scipy import stats
+from math import sqrt
 
 
 # Bivariate Data Analysis Functions
@@ -32,7 +34,7 @@ def scatter_plot(x, y, x_label='x', y_label='y', zero_line=False):
     return plt
 
 
-def lsrl(x, y, summarize=True, rounding=None):
+def lsrl(x, y, summarize=False, rounding=None):
     """
     Calculates the least squares regression line (LSRL) for two collections.
     Returns either a simple summary, or the model object itself.
@@ -41,23 +43,22 @@ def lsrl(x, y, summarize=True, rounding=None):
     y = np.array(y)
     model = LinearRegression().fit(x, y)
 
-    if not summarize:
-        return model
+    if summarize:
+        intercept = model.intercept_
+        slope = model.coef_[0]
 
-    intercept = model.intercept_
-    slope = model.coef_[0]
+        if rounding:
+            intercept = round(intercept, rounding)
+            slope = round(slope, rounding)
 
-    if rounding:
-        intercept = round(intercept, rounding)
-        slope = round(slope, rounding)
+        return {"intercept": intercept, "slope": slope}
 
-    return {"intercept": intercept, "slope": slope}
+    return model
 
 
-def regression_plot(x, y, x_label='x', y_label='y'):
+def regression_plot(x, y, model, x_label='x', y_label='y'):
     """Returns a basic regression line + scatter plot for two collections."""
     plt = scatter_plot(x, y, x_label, y_label)
-    model = lsrl(x, y, False)
 
     x = np.array(x).reshape((-1, 1))
     plt.plot(x, model.predict(x), color='blue')
@@ -72,11 +73,10 @@ def estimate(intercept, slope, explanatory_var):
     return intercept + slope * explanatory_var
 
 
-def residuals(x, y, rounding=None):
+def residuals(x, y, model, rounding=None):
     """
     Returns the residual (observed - expected) for a simple linear regression.
     """
-    model = lsrl(x, y, summarize=False)
     result = []
 
     for i, _ in enumerate(x):
@@ -90,14 +90,13 @@ def residuals(x, y, rounding=None):
     return result
 
 
-def residual_plot(x, y):
+def residual_plot(x, y, model):
     """Returns a residual plot for a regression."""
-    return scatter_plot(x, residuals(x, y), zero_line=True)
+    return scatter_plot(x, residuals(x, y, model), zero_line=True)
 
 
-def mse(x, y, rounding=None, k=1):
+def mse(x, y, model, k=1, rounding=None):
     """Returns the mean squared error (s^2) for a regression."""
-    model = lsrl(x, y, summarize=False)
     sse = 0
 
     n = len(x)
@@ -112,3 +111,53 @@ def mse(x, y, rounding=None, k=1):
         return round(result, rounding)
 
     return result
+
+
+def t_conf_linear(b1, se, n, k=1, conf_level=.95, rounding=None):
+    df = n - (k + 1)
+    t_crit = stats.t.ppf(conf_level + (1-conf_level)/2, df=df)
+
+    lower = b1 - t_crit * se
+    upper = b1 + t_crit * se
+
+    if rounding:
+        lower = round(lower, rounding)
+        upper = round(upper, rounding)
+
+    return (lower, upper)
+
+
+def se_linear(x, mse, rounding=None):
+    s = sqrt(mse)
+    sum_squares = 0
+    mean_x = np.mean(x)
+
+    for val in x:
+        sum_squares += (val - mean_x) ** 2
+
+    result = s / sqrt(sum_squares)
+
+    if rounding:
+        result = round(result, rounding)
+
+    return result
+
+
+def run_regression(x, y, conf_level=.95):
+    """Runs a simple linear regression analysis."""
+    model = lsrl(x, y)
+    s_2 = mse(x, y, model)
+    se = se_linear(x, s_2)
+    conf = t_conf_linear(model.coef_[0], se, len(x), conf_level=conf_level)
+
+    return {
+        "model": model,
+        "intercept": model.intercept_,
+        "slope": model.coef_[0],
+        "MSE": s_2,
+        "SE": se,
+        "conf_level": conf_level,
+        "conf_interval": conf,
+        "regression_plot": lambda: regression_plot(x, y, model).show(),
+        "residual_plot": lambda: residual_plot(x, y, model).show()
+    }
--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,6 @@
-from stats.stats import cor, lsrl, estimate, residuals, mse
+from stats.stats import (
+    cor, lsrl, estimate, residuals, mse, t_conf_linear, se_linear
+)
 from sklearn.linear_model import LinearRegression
 import math
 
@@ -14,21 +16,33 @@ def test_corr():
 
 
 def test_lsrl():
-    assert lsrl(
-        [1, 2, 2, 3],
-        [3, 4, 5, 6],
-        rounding=3) == {'intercept': 1.5, 'slope': 1.5}
+    x = [1, 2, 2, 3]
+    y = [3, 4, 5, 6]
+
+    model = lsrl(x, y)
+    assert isinstance(model, LinearRegression)
+    assert round(model.intercept_, 2) == 1.5
+    assert round(model.coef_[0], 2) == 1.5
 
     x = [6, 7, 7, 8, 10, 10, 11, 12, 14, 15, 16]
     y = [55, 40, 50, 41, 35, 28, 38, 32, 28, 18, 13]
 
-    assert lsrl(x, y, rounding=2) == {'intercept': 70.16, 'slope': -3.39}
-
-
-def test_lsrl_summarize_false():
-    model = lsrl([1, 2, 2, 3], [3, 4, 5, 6], False)
+    model = lsrl(x, y)
 
     assert isinstance(model, LinearRegression)
+    assert round(model.intercept_, 2) == 70.16
+    assert round(model.coef_[0], 2) == -3.39
+
+
+def test_lsrl_summarize():
+    x = [6, 7, 7, 8, 10, 10, 11, 12, 14, 15, 16]
+    y = [55, 40, 50, 41, 35, 28, 38, 32, 28, 18, 13]
+
+    assert lsrl(
+        x,
+        y,
+        summarize=True,
+        rounding=2) == {'intercept': 70.16, 'slope': -3.39}
 
 
 def test_estimate():
@@ -42,17 +56,49 @@ def test_estimate():
 def test_residuals():
     x = [1, 2, 2, 3]
     y = [3, 4, 5, 6]
+    model = lsrl(x, y)
 
-    assert residuals(x, y, 3) == [0.0, -0.5, 0.5, 0.0]
+    assert residuals(x, y, model, 3) == [0.0, -0.5, 0.5, 0.0]
 
 
 def test_mse():
     x = [1, 2, 2, 3]
     y = [3, 4, 5, 6]
 
-    assert mse(x, y, 3) == 0.25
+    model = lsrl(x, y)
+
+    assert mse(x, y, model, rounding=3) == 0.25
 
     x = [6, 7, 7, 8, 10, 10, 11, 12, 14, 15, 16]
     y = [55, 40, 50, 41, 35, 28, 38, 32, 28, 18, 13]
 
-    assert round(math.sqrt(mse(x, y)), 2) == 5.01
+    model = lsrl(x, y)
+
+    assert round(math.sqrt(mse(x, y, model)), 2) == 5.01
+
+
+def test_se_linear():
+    x = [1, 2, 2, 3]
+    mse = 0.25
+
+    assert se_linear(x, mse, 4) == 0.3536
+
+
+def test_t_conf_linear():
+    b1 = 0.7758
+    se = 0.1937
+    n = 10
+
+    assert t_conf_linear(b1, se, n, rounding=4) == (0.3291, 1.2225)
+
+    b1 = 1.5
+    x = [1, 2, 2, 3]
+    mse = 0.25
+    se = se_linear(x, mse)
+
+    assert t_conf_linear(
+        b1,
+        se,
+        len(x),
+        conf_level=.90,
+        rounding=2) == (0.47, 2.53)
